f6f6cd3841261ffd1be4a726b61b47e9e9d62d23
balazs4 <balazs4web@gmail.com>
2019-11-07 00:27:49 +0100

    feat: add basic navigation

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,34 @@
-import { request } from 'graphql-request';
-import useSWR from 'swr';
+import { useState } from 'react';
+import Link from 'next/link';
 
-function Index() {
-  const { data, error } = useSWR(
-    `query {
-      allSetsByElement(elementId: "4546207") {
-        name
-          sets {
-            name
-            set_num
-          }
-      }
-    }
-  `,
-    query => request('/api', query)
+function SearchInput(props) {
+  const [id, setId] = useState('');
+  return (
+    <>
+      <div>
+        <label htmlFor="input">{props.label}</label>
+        <input
+          type="text"
+          placeholder={props.placeholder}
+          value={id}
+          onChange={e => setId(e.target.value)}
+        />
+        <Link href={`${props.href}/${id}`}>
+          <a>Search</a>
+        </Link>
+      </div>
+      <style jsx>{``}</style>
+    </>
   );
-
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
 
-export default Index;
+export default () => (
+  <>
+    <div>
+      {[`allSetsByElement`].map(query => (
+        <SearchInput key={query} label={query} href={`/${query}`} />
+      ))}
+    </div>
+    <style jsx>{``}</style>
+  </>
+);
