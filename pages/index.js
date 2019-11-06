db2e985df171b2e78967922908c1307617b539b2
balazs4 <balazs4web@gmail.com>
2019-11-06 23:27:05 +0100

    feat: add swr

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import { request } from 'graphql-request';
+import useSWR from 'swr';
+
 function Index() {
-  return `Hello world`;
+  const { data, error } = useSWR(
+    `query {
+      allSetsByElement(elementId: "4546207") {
+        name
+          sets {
+            name
+            set_num
+          }
+      }
+    }
+  `,
+    query => request('/api', query)
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
 
 export default Index;
